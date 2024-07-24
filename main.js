d36f6c14cbe1028cd505967d3fc3306198cd0148
notdavido <153490255+notdavido@users.noreply.github.com>
2024-07-24 13:52:16 -0400

    structure change

--- main.js
+++ main.js
@@ -13,31 +13,27 @@ const firebaseConfig = {
     databaseURL: `https://websitequoteproj-default-rtdb.firebaseio.com/`,
   };
 
-
   //initialize
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();
-
 const auth = getAuth();
-
 
 function validateemail(email) {
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
-     {
-       return true
-     }
-       alert("You have entered an invalid email address!")
-       return false
-   }
-   
-   function validatepassword(password) {
-    if (password < 6) {
-      return false
-    } else {
-      return true
+        {
+        return true
+        }
+        alert("You have entered an invalid email address!")
+        return false
     }
-  }
-  
+
+    function validatepassword(password) {
+        if (password < 6) {
+            return false
+          } else {
+            return true
+          }
+    }
 
 async function register(event) {
     event.preventDefault();
